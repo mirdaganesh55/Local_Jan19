e2e232fec9db45ec2af7ab9dba35d48706e6e4a1
mirdaganesh55 <ganeshmirda5@gmail.com>
2024-01-22 10:31:02 +0530

    pack5

---
+++ src/main/java/com/java/pack5/ClassA.java
@@ -0,0 +1,8 @@
+package com.java.pack5;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("Rahul is Raman...");
+	}
+}
---
+++ src/main/java/com/java/pack5/Welcome.java
@@ -0,0 +1,8 @@
+package com.java.pack5;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		System.out.println("Java is Awesome...");
+	}
+}
